326083322099c5d043a242f694f59d1c5e5c16a0
Maxpower1711 <estefanoh_13@hotmail.com>
2025-04-26 21:19:07 -0500

    Se agrego la variable edad y el KG

--- src/view/JFMascota.java
+++ src/view/JFMascota.java
@@ -17,6 +17,9 @@ public class JFMascota extends javax.swing.JFrame {
     int getCodigo(){
         return Integer.parseInt(txtCodigo.getText());
     }
+    int getEdad(){
+        return Integer.parseInt(txtEdad.getText());
+    }
     String getRaza(){
         return txtRaza.getText();
     }
@@ -46,10 +49,10 @@ public class JFMascota extends javax.swing.JFrame {
     void listar(){
         imprimir();
         if (m.tamaño()>0) {
-            imprimir("codigo \t Raza \t\t Peso \t Nro. de vacunas");
+            imprimir("codigo \t Edad \t Raza \t\t Peso \t Nro. de vacunas");
             for (int i = 0; i < m.tamaño(); i++) {
                 Mascota masco=m.obtener(i);
-                imprimir(masco.getCodigo()+"\t"+masco.getRaza()+"\t\t"+
+                imprimir(masco.getCodigo()+"\t"+masco.getEdad()+"\t"+masco.getRaza()+"\t\t"+
                         masco.getPeso()+"\t\t"+masco.getNumeroVacunas());
             }
             imprimir("");
@@ -62,7 +65,7 @@ public class JFMascota extends javax.swing.JFrame {
     void ingresar(){
         Mascota masco=m.buscar(getCodigo());
         if (masco==null) {
-            masco = new Mascota(getCodigo(),getRaza(),getPeso(),getNumeroVacunas());
+            masco = new Mascota(getCodigo(),getEdad(),getRaza(),getPeso(),getNumeroVacunas());
             m.adicionar(masco);
             listar();
             mensaje("Mascota Ingresada", "Mensaje de confirmacion");
@@ -76,6 +79,7 @@ public class JFMascota extends javax.swing.JFrame {
         Mascota masco=m.buscar(getCodigo());
         if (masco!=null) {
             imprimir("Codigo            :"+masco.getCodigo());
+            imprimir("Edad         :"+masco.getEdad());
             imprimir("Raza       :"+masco.getRaza());
             imprimir("Peso   :"+masco.getPeso());
             imprimir("Nro. de vacunas             :"+masco.getNumeroVacunas());
@@ -87,6 +91,7 @@ public class JFMascota extends javax.swing.JFrame {
     void modificar(){
         Mascota masco=m.buscar(getCodigo());
         if (masco!=null) {
+            masco.setEdad(getEdad());
             masco.setRaza(getRaza());
             masco.setPeso(getPeso());
             masco.setNumeroVacunas(getNumeroVacunas());
@@ -110,6 +115,7 @@ public class JFMascota extends javax.swing.JFrame {
     //metodo borrar
     void borrar(){
         txtCodigo.setText("");
+        txtEdad.setText("");
         txtRaza.setText("");
         txtPeso.setText("");
         txtNumero.setText("");
@@ -129,15 +135,23 @@ public class JFMascota extends javax.swing.JFrame {
     void seleccionar(){
         int opcion=cboOpcion.getSelectedIndex();
         if (opcion==1 || opcion==3) {
+            txtEdad.setVisible(false);
+            lblEdad.setVisible(false);
             txtRaza.setVisible(false);
+            lblRaza.setVisible(false);
             lblPeso.setVisible(false);
             txtPeso.setVisible(false);
+            lblKg.setVisible(false);
             lblNumero.setVisible(false);
             txtNumero.setVisible(false);
         }else{
+            txtEdad.setVisible(true);
+            lblEdad.setVisible(true);
             txtRaza.setVisible(true);
+            lblRaza.setVisible(true);
             lblPeso.setVisible(true);
             txtPeso.setVisible(true);
+            lblKg.setVisible(true);
             lblNumero.setVisible(true);
             txtNumero.setVisible(true);
         }
@@ -156,7 +170,7 @@ public class JFMascota extends javax.swing.JFrame {
         bandeja = new javax.swing.JTextArea();
         jLabel1 = new javax.swing.JLabel();
         lblCodigo = new javax.swing.JLabel();
-        lblDescripcion = new javax.swing.JLabel();
+        lblRaza = new javax.swing.JLabel();
         lblPeso = new javax.swing.JLabel();
         lblNumero = new javax.swing.JLabel();
         cboOpcion = new javax.swing.JComboBox<>();
@@ -166,6 +180,9 @@ public class JFMascota extends javax.swing.JFrame {
         btnProcesar = new javax.swing.JButton();
         btnCerrar = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
+        lblEdad = new javax.swing.JLabel();
+        txtEdad = new javax.swing.JTextField();
+        lblKg = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -177,7 +194,7 @@ public class JFMascota extends javax.swing.JFrame {
 
         lblCodigo.setText("Codigo de la mascota:");
 
-        lblDescripcion.setText("Raza:");
+        lblRaza.setText("Raza:");
 
         lblPeso.setText("Peso:");
 
@@ -214,44 +231,68 @@ public class JFMascota extends javax.swing.JFrame {
         jLabel2.setFont(new java.awt.Font("Dialog", 0, 24)); // NOI18N
         jLabel2.setText("REGISTRO VETERINARIA");
 
+        lblEdad.setText("Edad:");
+
+        lblKg.setText("KG.");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(77, 77, 77)
+                        .addGap(120, 120, 120)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(lblNumero)
-                            .addComponent(lblPeso)
-                            .addComponent(lblDescripcion)
-                            .addComponent(lblCodigo)
-                            .addComponent(jLabel1))
-                        .addGap(26, 26, 26)
+                            .addComponent(jLabel1)
+                            .addComponent(lblNumero))
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
+                                .addGap(26, 26, 26)
                                 .addComponent(cboOpcion, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(16, 142, Short.MAX_VALUE))
                             .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(txtCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                        .addComponent(txtPeso, javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(txtNumero, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addComponent(txtRaza, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 74, Short.MAX_VALUE)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(btnProcesar)
-                                    .addComponent(btnCerrar)
-                                    .addComponent(btnLimpiar)))))
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGap(28, 28, 28)
+                                .addComponent(txtNumero, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(164, 164, 164)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(lblPeso)
+                            .addComponent(lblRaza))
+                        .addGap(27, 27, 27)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(txtPeso, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(lblKg, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(txtRaza, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
                             .addGap(122, 122, 122)
                             .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 298, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGroup(layout.createSequentialGroup()
                             .addGap(38, 38, 38)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 448, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 448, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(79, 79, 79)
+                                .addComponent(lblCodigo)
+                                .addGap(26, 26, 26))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(lblEdad, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtCodigo, javax.swing.GroupLayout.DEFAULT_SIZE, 73, Short.MAX_VALUE)
+                            .addComponent(txtEdad))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnProcesar)
+                            .addComponent(btnCerrar)
+                            .addComponent(btnLimpiar))))
                 .addContainerGap(35, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -263,31 +304,38 @@ public class JFMascota extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(cboOpcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(lblCodigo)
-                            .addComponent(txtCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(lblDescripcion)
-                            .addComponent(txtRaza, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(lblPeso)
-                            .addComponent(txtPeso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
                         .addComponent(btnLimpiar)
                         .addGap(18, 18, 18)
                         .addComponent(btnProcesar)
                         .addGap(18, 18, 18)
-                        .addComponent(btnCerrar)))
-                .addGap(18, 18, 18)
+                        .addComponent(btnCerrar))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(6, 6, 6)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblCodigo)
+                            .addComponent(txtCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtEdad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblEdad))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblRaza)
+                            .addComponent(txtRaza, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtPeso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblPeso)
+                            .addComponent(lblKg))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblNumero)
                     .addComponent(txtNumero, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                .addGap(30, 30, 30)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18))
         );
@@ -353,10 +401,13 @@ public class JFMascota extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblCodigo;
-    private javax.swing.JLabel lblDescripcion;
+    private javax.swing.JLabel lblEdad;
+    private javax.swing.JLabel lblKg;
     private javax.swing.JLabel lblNumero;
     private javax.swing.JLabel lblPeso;
+    private javax.swing.JLabel lblRaza;
     private javax.swing.JTextField txtCodigo;
+    private javax.swing.JTextField txtEdad;
     private javax.swing.JTextField txtNumero;
     private javax.swing.JTextField txtPeso;
     private javax.swing.JTextField txtRaza;
